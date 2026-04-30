a090c25fbef3a15619676c2fb3adc2bdf79865e0
by10granda <by10granda@github.com>
2026-04-29 20:36:11 -0500

    Fix: Wave positioned with translate-y-full to hang below yellow section, showing all cards

--- src/pages/QuienesSomos.tsx
+++ src/pages/QuienesSomos.tsx
@@ -275,9 +275,9 @@ export const QuienesSomos = () => {
           </div>
 
           {/* Ola SVG inferior */}
-          <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none" style={{ lineHeight: 0 }}>
+          <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none translate-y-full" style={{ lineHeight: 0 }}>
             <svg viewBox="0 0 1440 150" preserveAspectRatio="none" className="w-full block" style={{ display: 'block' }}>
-              <path d="M0,75 Q360,150 720,75 T1440,75 L1440,150 L0,150 Z" fill="#ffffff" />
+              <path d="M0,0 Q360,150 720,0 T1440,0 L1440,150 L0,150 Z" fill="#ffffff" />
             </svg>
           </div>
         </section>
