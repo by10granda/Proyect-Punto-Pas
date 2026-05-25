c2ada3f39098099909e1c8b8fce6c259a97145f2
by10granda <by10granda@github.com>
2026-05-25 16:00:52 -0500

    Merge E and _E image variants in product gallery dedup

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -580,7 +580,9 @@ export const ProductView = ({ product, onAddToCart, onClose }: ProductViewProps)
     const toImageIdentityKey = (imageUrl: string): string => {
       const withoutQuery = imageUrl.split("?")[0].toLowerCase();
       const withoutExtension = withoutQuery.replace(/\.(png|jpg|jpeg|webp|gif)$/i, "");
-      const normalizedSuffix = withoutExtension.replace(/_e\d*$/i, "_e");
+      const normalizedSuffix = withoutExtension
+        .replace(/_?e\d*$/i, "_e")
+        .replace(/_+/g, "_");
       return normalizedSuffix;
     };
 
